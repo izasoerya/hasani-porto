09c6ce1864bf192c1209d27c7e0288f2ec4bb2d3
izasoerya <ihza.surya@gmail.com>
2024-08-25 16:00:18 +0700

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className="xs:w-[250px] w-full">
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-white rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+      >
+        <img
+          src={icon}
+          alt="web-development"
+          className="w-16 h-16 object-contain"
+        />
+
+        <h3 className="text-[#405D72] text-[20px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I am a highly motivated and results-driven professional with a diverse
+        background in research, innovation, and technology development. As a
+        fresh graduate with experience in product management, I have accumulated
+        valuable experiences in managing and leading teams, conceptualizing
+        cutting-edge designs, and successfully implementing projects across
+        various sectors. My expertise lies in instrumentation technology, IoT,
+        artificial intelligence, and electrical engineering.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
---
+++ src/constants/index.js
@@ -0,0 +1,365 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  BCA,
+  frogs,
+  starbucks,
+  KMB,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  DEMA,
+  PERMI,
+  UIN,
+  BRIN,
+  BUDAK,
+  GANA,
+  KANAL,
+  SEARCH,
+  ALPIT,
+  KOPMA,
+  VIDEO,
+  baznas,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Experience",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+  {
+    id: "LinkedIn",
+    title: "LinkedIn",
+    url: "https://www.linkedin.com/in/ahmad-ghozali-7293ba280?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+  },
+];
+
+const services = [
+  {
+    title: "Website and IoT Developer",
+    icon: web,
+  },
+  {
+    title: "Electrical Engineer",
+    icon: mobile,
+  },
+  {
+    title: "AI Engineer",
+    icon: meta,
+  },
+  {
+    title: "Project Manager",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Asisten Peneliti Badan Riset dan Inovasi Nasional (BRIN) RI",
+    company_name: "BRIN Yogyakarta",
+    icon: BRIN,
+    iconBg: "#383E56",
+    date: "July 2023 - Now",
+    points: [
+      "Managing MBKM students in making disaster technology prototypes.",
+      "Conceptualize the design of disaster technology based on IoT and Artificial Intelligence.",
+      "Prepared a Budget Plan for the construction of prototypes..",
+      "Respond to comments, messages and feedback from the audience quickly and professionally.",
+      "Creating a prediction model based on historical data and environmental variables.",
+    ],
+  },
+  {
+    title: "Product Manager and RND Specialist",
+    company_name: "BRIN Bogor",
+    icon: BRIN,
+    iconBg: "#E6DEDD",
+    date: "August 2023 - January 2024",
+    points: [
+      "Effectively led three project areas: Air Quality Monitoring, Water Quality Monitoring, and Early Detection of Landslide And Flood Disasters.",
+      "Efficiently planned the conceptual framework for the development of prototypes for three projects to be implemented.",
+      "Accomplished coordination with the Executive Manager of BRIN for the commercialization of the project.",
+      "Successfully conceptualized the development of a website and application for the prototypes.",
+      "Secured patents for the three projects.",
+    ],
+  },
+  {
+    title: "RND Manager",
+    company_name: "PT Frogs Indonesia",
+    icon: frogs,
+    iconBg: "#383E56",
+    date: "August 2023 - Jan 2024",
+    points: [
+      "Efficiently led the team in prototype development.",
+      "Successfully executed the implementation of the forest and land fire detection drone prototype.",
+      "Pioneered the conceptualization of a ground control station in forest and land fire monitoring.",
+      "Designed PCB and Executed the Manufacture of Electronics tester system.",
+      "Successfully secured a patent for Forest and Land Fire Detection Drone.",
+    ],
+  },
+  {
+    title: "Awardee Badan Amil Zakat Nasional Scholarship",
+    company_name: "Badan Amil Zakat Nasional (BAZNAS)",
+    icon: baznas,
+    iconBg: "#E6DEDD",
+    date: "September 2021 - October 2023",
+    points: [
+      "Time Management and Organization Training for Balancing Studies with Program Activities",
+      "Leadership and Communication Training through Engagement in Mentoring and Group Discussions",
+      "Critical Thinking and Problem-Solving Training through Community Service Projects",
+    ],
+  },
+  {
+    title: "Awardee Bakti BCA Development Program and Scholarship",
+    company_name: "Bank Central Asia (BCA)",
+    icon: BCA,
+    iconBg: "#E6DEDD",
+    date: "August 2022 - September 2023",
+    points: [
+      "Team Collaboration Training, fostering a culture of collaboration and building high-performing teams.",
+      "Time Management Training, providing individuals with practical strategies for optimal productivity.",
+      "Comprehensive Critical Thinking Training, enhancing analytical skills and problem-solving abilities.",
+      "Leadership Training with a focus on various leadership styles.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Game Asset Nusantara",
+    description:
+      "Website-based game asset sales platform built using the Laravel framework, with Ahmad Hamam and Farhan Iqbal during their internship at BRIN RI",
+    tags: [
+      {
+        name: "laravel",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "MySQL",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Bootsrap",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: GANA,
+    source_code_link: "https://github.com/gghozzit/SellingAssetsNusantara.git",
+  },
+  {
+    name: "Kanal Berita",
+    description:
+      "A website-based news platform built using PHP and MySQL as the database, a display is available for users and an admin dashboard used to manage content on news channels.",
+    tags: [
+      {
+        name: "PHP",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "MySQL",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Bootsrap",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: KANAL,
+    source_code_link: "https://github.com/gghozzit/KanalBerita.git",
+  },
+  {
+    name: "Tahu Seacrh",
+    description:
+      "A program in the form of a search column using various algorithms (vector Space Model, Cosine Similarity, Extended Boolean, Naive Bayes, KNN) based on HTML and CSS",
+    tags: [
+      {
+        name: "Python",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "CSS",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Dataset",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: SEARCH,
+    source_code_link:
+      "https://github.com/gghozzit/SearchWithVariousAlgorithms.git",
+  },
+  {
+    name: "Alpit Shooters",
+    description:
+      "Alpit Shooter is a Third Person Shooter (TPS) Windows platform based where set in carnaval event. Player aim to score point by hit on military target in game.",
+    tags: [
+      {
+        name: "Unity",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Blender",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Illustrator",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: ALPIT,
+    source_code_link: "https://github.com/gghozzit/AlpitShooter_Game.git",
+  },
+  {
+    name: "Kopma Application",
+    description:
+      "Multi-Platform sales application built using Flutter with the Dart language, this application provides sales, purchase, and transaction.",
+    tags: [
+      {
+        name: "Flutter",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Dart",
+        color: "green-text-gradient",
+      },
+      {
+        name: "AndroidStudio",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: KOPMA,
+    source_code_link: "https://github.com/gghozzit/kopma.git",
+  },
+  {
+    name: "My Other Project Results",
+    description:
+      "My other work is in the form of video editing using the Premiere Pro, Davinci, and After Effects applications. In the form of Aftermovie, Profile, Testimonial, and many more.",
+    tags: [
+      {
+        name: "PremierePro",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Davinci",
+        color: "green-text-gradient",
+      },
+      {
+        name: "AfterEffects",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: VIDEO,
+    source_code_link:
+      "https://drive.google.com/drive/folders/11K2cBeobQY35niWnlAu4xSzCMmjcaGdP?usp=drive_link",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
